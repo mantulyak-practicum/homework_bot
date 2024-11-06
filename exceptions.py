221e3f5b16aa685993bf133d2af79e656fbb5c18
Igor Mantuliak <igor.mantuliak@my.games>
2024-11-07 00:05:41 +0100

    fixes after review

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,11 @@ class HomeworkKeyError(Exception):
 
 class HomeworkApiError(Exception):
     """Вызывается при ошибках при запросе к API."""
+
+
+class SendMessageError(Exception):
+    """Вызывается при ошибках отправки сообщения в ТГ."""
+
+
+class HomeworkResponseError(Exception):
+    """Вызывается при получении неожиданных данных в ответе."""
--- homework.py
+++ homework.py
@@ -2,14 +2,25 @@ import logging
 from logging.handlers import RotatingFileHandler
 import os
 import requests
+from requests.exceptions import RequestException
 import sys
 import time
 
 from dotenv import load_dotenv
 from telebot import TeleBot
+from telebot.apihelper import ApiException
 
-from exceptions import (VariableMissing, HomeworkKeyError, HomeworkApiError)
-from handlers import telegram_handler
+from exceptions import (
+    VariableMissing,
+    HomeworkKeyError,
+    HomeworkApiError,
+    SendMessageError,
+    HomeworkResponseError
+)
+# Импорт telegram_handler использовался при добавлении хендлера к логгеру.
+# Логика про то, что не доложно отсылаться два сообщения была тоже
+# реализована в хендлере, так что ТЗ все же было выполнено)
+# В этой версии убрал его, сделал отправку в ТГ с помощью send_message()
 
 
 load_dotenv()
@@ -33,40 +44,29 @@ LOGS_PARAM = {
     'BACKUP_COUNT': 5,
     'FORMAT': '%(asctime)s - %(levelname)s: %(message)s',
 }
-logging.basicConfig(
-    level=logging.DEBUG,
-    format=LOGS_PARAM['FORMAT'],
-    handlers=[
-        logging.StreamHandler(stream=sys.stdout),
-        RotatingFileHandler(
-            filename=LOGS_PARAM['FILENAME'],
-            mode=LOGS_PARAM['MODE'],
-            maxBytes=LOGS_PARAM['MAX_BYTES'],
-            backupCount=LOGS_PARAM['BACKUP_COUNT'],
-        ),
-    ],
-)
-homework_logger = logging.getLogger(__name__)
-homework_logger.addHandler(telegram_handler)
 
 
 def check_tokens():
     """Проверяет наличие необходимых переменных для работы бота."""
-    for token in ('PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID'):
-        if not globals()[token]:
-            msg = f'Проверьте доступность переменной {token}.'
-            homework_logger.critical(msg)
-            raise VariableMissing(msg)
+    missing_tokens = [
+        token for token in (
+            'PRACTICUM_TOKEN', 'TELEGRAM_TOKEN', 'TELEGRAM_CHAT_ID'
+        ) if not globals()[token]
+    ]
+    if missing_tokens:
+        return (
+            f'Проверьте доступность переменных: {", ".join(missing_tokens)}.'
+        )
 
 
 def send_message(bot, message):
     """Отправляет сообщение message в чат телеграма."""
     try:
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message,)
-    except Exception as error:
-        homework_logger.error(f'Не удалось отправить сообщение - {error}')
+    except (ApiException, RequestException) as error:
+        raise SendMessageError(f'Не удалось отправить сообщение - {error}')
     else:
-        homework_logger.debug(
+        logging.debug(
             f'Отправлено сообщение "{message}" в чат {TELEGRAM_CHAT_ID}'
         )
 
@@ -74,82 +74,114 @@ def send_message(bot, message):
 def get_api_answer(timestamp):
     """Возвращает ответ на запрос к api Яндекс в формате словаря Python."""
     try:
-        response = requests.get(
-            url=ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
-        )
-    except requests.exceptions.RequestException as error:
-        homework_logger.error(f'Ошибка запроса к API – {error}.')
-        return error
-    else:
+        request_kwargs = {
+            'url': ENDPOINT,
+            'headers': HEADERS,
+            'params': {'from_date': timestamp},
+        }
+        response = requests.get(**request_kwargs)
         if response.status_code == 400:
-            msg = 'Неверное значение from_date'
-            homework_logger.error(msg)
-            raise HomeworkApiError(msg)
-        elif response.status_code == 401:
-            msg = 'Неверное значение PRACTICUM_TOKEN'
-            homework_logger.error(msg)
-            raise HomeworkApiError(msg)
-        elif response.status_code != 200:
-            homework_logger.error(
-                f'Статус ответа API не 200. Код ответа: {response.status_code}'
+            raise HomeworkApiError(
+                f'Неверное значение from_date. {request_kwargs}'
             )
-            raise requests.exceptions.HTTPError
+        elif response.status_code == 401:
+            raise HomeworkApiError(
+                f'Неверное значение PRACTICUM_TOKEN. {request_kwargs}'
+            )
+        elif response.status_code != 200:
+            raise HomeworkApiError(
+                f'Статус ответа API не 200. Код ответа: {response.status_code}'
+                f'Параметры запроса: {request_kwargs}'
+            )
         return response.json()
+    except RequestException as error:
+        raise HomeworkApiError(
+            f'Ошибка запроса к API – {error}. '
+            f'Параметры запроса: {request_kwargs}'
+        ) from error
 
 
 def check_response(response):
     """Проверяет что в ответе данные соответствуют ожидаемым типам."""
-    if type(response) is not dict:
-        msg = f'Ответ от API не словарь. Тип ответа – {type(response)}'
-        homework_logger.error(msg)
-        raise TypeError(msg)
-    if type(response.get('homeworks')) is not list:
-        msg = (
-            'Домашки передаются не списком, '
-            f'а {type(response.get("homeworks"))}'
+    if not isinstance(response, dict):
+        raise TypeError(
+            f'Ответ от API не словарь. Тип ответа – {type(response)}'
         )
-        homework_logger.error(msg)
-        raise TypeError(msg)
+    elif response.get('homeworks') is None:
+        raise HomeworkResponseError(
+            'В ответе нет ключа "homeworks".'
+        )
+    elif not isinstance(response['homeworks'], list):
+        raise TypeError(
+            'Домашки передаются не списком, '
+            f'а {type(response["homeworks"])}'
+        )
 
 
 def parse_status(homework):
     """Извлекает данные из словаря и готовит сообщение для отправки в TG."""
     if 'homework_name' not in homework:
-        msg = 'В ответе отсутствует имя домашки'
-        homework_logger.error(msg)
-        raise HomeworkKeyError(msg)
+        raise HomeworkKeyError('В ответе отсутствует имя домашки')
+    elif 'status' not in homework:
+        raise HomeworkKeyError('В ответе отсутствует статус домашки')
     elif homework.get('status') not in HOMEWORK_VERDICTS:
-        msg = 'В ответе неожиданный статус домашки'
-        homework_logger.error(msg)
-        raise HomeworkKeyError(msg)
-    else:
-        homework_name = homework['homework_name']
-        return str(
-            f'Изменился статус проверки работы "{homework_name}". '
-            f'{HOMEWORK_VERDICTS[homework["status"]]}'
+        raise HomeworkKeyError(
+            f'В ответе неожиданный статус домашки: {homework.get("status")}'
         )
+    homework_name = homework['homework_name']
+    return str(
+        f'Изменился статус проверки работы "{homework_name}". '
+        f'{HOMEWORK_VERDICTS[homework["status"]]}'
+    )
 
 
 def main():
     """Основная логика работы бота."""
-    check_tokens()
+    if check_tokens() is not None:
+        logging.critical(check_tokens())
+        raise VariableMissing(check_tokens())
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
     while True:
         try:
             api_response = get_api_answer(timestamp)
             check_response(api_response)
-            try:
-                homework = api_response['homeworks'][0]
-                send_message(bot, parse_status(homework))
-                timestamp = api_response.get('current_date', timestamp)
-            except IndexError:
-                homework_logger.debug('Пока нет ответа от ревьювера')
+            homeworks = api_response['homeworks']
+            if len(homeworks) > 0:
+                new_message = parse_status(homeworks[0])
+                if globals().get('status_message') != new_message:
+                    status_message = new_message
+                    send_message(bot, status_message)
+            else:
+                logging.debug('В ответе нет данных о домашке')
+            timestamp = api_response.get('current_date', timestamp)
         except Exception as error:
-            homework_logger.error(error)
+            logging.error(
+                f'{type(error).__name__} — {error}'
+            )
+            try:
+                if globals().get('error_message') != error:
+                    error_message = error
+                    send_message(bot, error_message)
+            except SendMessageError as error:
+                logging.error(f'{error}')
         finally:
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format=LOGS_PARAM['FORMAT'],
+        encoding='utf-8',
+        handlers=[
+            logging.StreamHandler(stream=sys.stdout),
+            RotatingFileHandler(
+                filename=LOGS_PARAM['FILENAME'],
+                mode=LOGS_PARAM['MODE'],
+                maxBytes=LOGS_PARAM['MAX_BYTES'],
+                backupCount=LOGS_PARAM['BACKUP_COUNT'],
+            ),
+        ],
+    )
     main()
